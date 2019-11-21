a4b57bd904eb4b2fbec1033c5efcb57b539215fa
Noel Khalaf <nkhal049@uottawa.ca>
2019-11-21 15:21:25 -0500

    Third times the charm i guess

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -20,7 +20,7 @@ public class ProductTest {
     public void checkProductPrice() {
         // I am making this test work!
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the price of the product", 180, aProduct.getPrice());
+        assertEquals("180", aProduct.getPrice());
     }
 
 
